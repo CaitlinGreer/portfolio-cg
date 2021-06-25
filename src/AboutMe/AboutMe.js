6596e6f1f7e1c83f8c0fb038e287290fce6678bd
CaitlinGreer <clento1121@gmail.com>
2021-06-25 17:37:06 -0400

    added skills section

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Skills from '../Skills/Skills'
+
 import './AboutMe.css'
 
 import travel from '../images/travelphoto.jpeg'
@@ -11,14 +13,27 @@ export default class AboutMe extends Component {
                     <div className='about-heading'>
                         <h3>A Little About Myself</h3>
                     </div>
-                    <p>After working as a hairstylist for most of my life I felt it was time for a change.  So I chose to hang up the shears and pick up the keyboard.  I've always had an interest in code and programming and there's no time like the present for a new career adventure.</p>
+                    <p>After working as a hairstylist for most of my life I felt it was time for a change.
+                        So I chose to hang up the shears and pick up the keyboard.  I've always had an 
+                        interest in code and programming and there's no time like the present for a new 
+                        career adventure.</p>
 
                     <p>My adventure into learning web development at <a href="https://bloc.thinkful.com/" >Bloc/Thinkful</a> has
-                         been one of the most challenging and rewarding things I've ever done.  We all know that teamwork makes the dreamwork, so I'm looking forward to working on a team with other programmers and truly experiencing everything the industry has to offer.
+                         been one of the most challenging and rewarding things I've ever done.  
+                         We all know that teamwork makes the dreamwork, so I'm always looking to work with a team or 
+                         collborate with other developers and truly experiencing everything the industry has to offer.
                     </p>
-                    <p>If you don't find me at my desk, you'll most likely find me bouncing around Disney World soaking up some Disney magic, spending some quality time with my two dogs or traveling anywhere I possibly can.</p>
-                    <p>I provide a wide range of services from back end to front end developement, so let's build some things!  Reach out and let's talk about your project!
+                    <p>If you don't find me at my desk, you'll most likely find me bouncing around Disney World soaking 
+                        up some Disney magic, button smashing on my Xbox, spending some quality time with my two dogs or 
+                        traveling anywhere I possibly can.</p>
+                                       
+                    <p>I provide a wide range of services from back end to front end developement, so let's build some things!
+                       Reach out and let's talk about your project!
                     </p>
+                    <p>My Skills include but aren't limited to:
+                        <Skills />
+                        
+                    </p> 
                 </div>
                 <div className="travel-img">
                     <img src={travel} alt="map and camera" className='travel-image' />
---
+++ src/Skills/Skills.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+import './Skills.css'
+export default class Skills extends Component {
+    render() {
+        return (
+            <div>
+                <div className='skills-container'>
+                    <div>
+                        <p>JavaScript</p>
+                    </div>                    
+                    
+                    <div>
+                        <p>HTML</p>
+                    </div>
+                    <div>
+                        
+                        <p>CSS</p>
+                    </div>
+
+                    <div>
+                        
+                        <p>React</p>
+                    </div>
+                    <div>
+                        
+                        <p>Node.js</p>
+                    </div>
+                    <div>
+                        
+                        <p>Git</p>
+                    </div>
+                    <div>
+                        
+                        <p>jQuery</p>
+                    </div>
+                    <div>
+                       
+                        <p>Postgres</p>
+                    </div>
+                    <div>
+                        
+                        <p>SQL</p>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
