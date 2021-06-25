20bba909b9158145ea169299ab1555727b618a3a
CaitlinGreer <clento1121@gmail.com>
2021-06-25 16:59:53 -0400

    moved links to page

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-
 import './Footer.css'
 
 
@@ -10,21 +7,10 @@ class Footer extends React.Component {
         return (
             <div className='footer-container'>
                 <p>Created by Caitlin Greer</p>
-                <ul>
-                    <li>
-                        <a href='https://github.com/CaitlinGreer'>
-                            <FontAwesomeIcon icon={faGithubSquare} className='github' />
-                        </a>
-                    </li>
-                    <li>
-                        <a href='https://www.linkedin.com/in/caitlingreer/'>
-                            <FontAwesomeIcon icon={faLinkedin} className='linked-in' />
-                        </a>
-                    </li>
-                </ul>
+                
             </div>
         )
     }
-}
+} 
 
 export default Footer;
