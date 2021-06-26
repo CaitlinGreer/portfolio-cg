b391612d5d9c2ee230d3d84e8561652aeef192e7
CaitlinGreer <clento1121@gmail.com>
2021-06-25 22:14:35 -0400

    edited welcome info

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -17,9 +17,11 @@ export default class HomePage extends Component {
                     <h2>Hey!  I'm Caitlin Greer</h2>
 
                     <h3>Coming at you live from Orlando, Florida</h3>
-                    <p>As a new kid in the web developer world I was drawn to the idea of being able to create something from nothing.
-                        I'm fortunate enough to have had a career working face to face with people in a creative industry. Being able to
-                        take an idea and make something that exceeds client expectations every time is, what I like to call, my super power.
+                    <p>As a new kid in the web developer world I find myself drawn to the concept of creating something from nothing.
+                        I have been fortunate enough to spend most of my adult life working face to face with people in a creative industry. 
+                        The years I spent working in such an environment provided me with, what I often refer to as, my super power.  And what 
+                        power is that you ask?  It's the ability to take an idea and create something that exceeds client expectations
+                        every time.
                     </p>
 
                 </div>
