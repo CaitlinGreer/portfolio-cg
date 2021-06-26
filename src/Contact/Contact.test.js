820f3d889a338c2886ffefee95016e4254c88375
CaitlinGreer <clento1121@gmail.com>
2021-06-25 22:11:06 -0400

    added basic tests

--- src/Contact/Contact.test.js
+++ src/Contact/Contact.test.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import Contact from './Contact'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(
     <BrowserRouter>
-      <App />
-    </BrowserRouter>, 
-    div);
+      <Contact />
+    </BrowserRouter>,
+    div)
   ReactDOM.unmountComponentAtNode(div)
-});
+})
