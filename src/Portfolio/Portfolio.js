6f0e67f9da1c83e7eaa5292fc32c26d5f8c14d4b
CaitlinGreer <clento1121@gmail.com>
2021-06-25 22:33:06 -0400

    added tech info to projects

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -17,7 +17,14 @@ export default class AboutMe extends Component {
                         <span className="app-description">
                             <p>Magical Meals is a client-side React web app that allows users with dietary restrictions find meals based on park location at Walt Disney World.
                                 It was built to use an API, which can be viewed in the link below.
+                                <br /><br />
+                                <span className='built-with'>
+                                    TECH USED:
+                                    <br></br>
+                                    HTML5 | CSS | JavaScript | React | Node.js | Express | PostgreSQL | Knex.js | RESTful APIs</span>
+
                             </p>
+
                         </span>
 
                         <div className='links-section'><span className='check-it-out'>Check It Out Here</span>
@@ -35,7 +42,13 @@ export default class AboutMe extends Component {
                             <p>Chuckles is a client-side React web app created to bring a laugh to your day, that allows users to
                                 see and share dad jokes.
                                 It was built to use a back end server, which can be viewed in the link below.
+                                <br /><br />
+                                <span className='built-with'>
+                                    TECH USED:
+                                    <br></br>
+                                    HTML5 | CSS | JavaScript | React | Node.js | Express | PostgreSQL | Knex.js | RESTful APIs</span>
                             </p>
+
                         </span>
 
                         <div className='links-section'><span className='check-it-out'>Check It Out Here</span>
@@ -44,27 +57,35 @@ export default class AboutMe extends Component {
                                 <li><a href="https://github.com/CaitlinGreer/chuckles-app.git">GitHub Repo</a></li>
                                 <li><a href="https://github.com/CaitlinGreer/chuckles-api.git">API Repo</a></li>
                             </ul>
+
                         </div>
+
                     </div>
-                 
+
                     <div className='portfolio-item-3'>
                         <h3 className='app-heading'>Cocktail Hour</h3>
                         <img className="portfolio-image" src={cocktailHour} alt="cocktail hour home page" />
                         <span className='app-description'>
                             <p>An app created using JavaScript that makes API calls to The Cocktail DB API.
-                            The user can find a drink recipe by searching by ingredient, or walk on the wild side and choose a drink at random.
+                                The user can find a drink recipe by searching by ingredient, or walk on the wild side and choose a drink at random.
+                                <br /><br />
+                                <span className='built-with'>
+                                    TECH USED:
+                                    <br></br>
+                                    HTML5 | CSS | JavaScript | jQuery | RESTful APIs
+                                </span>
                             </p>
                         </span>
                         <div className='links-section'><span className='check-it-out'>Check It Out Here</span>
-                                <ul className='portfolio-links'>
-                                    <li><a href="https://caitlingreer.github.io/CocktailHour/">Live</a></li>
-                                    <li><a href="https://github.com/CaitlinGreer/CocktailHour.git">GitHub Repo</a></li>
-                                </ul>
-                            </div>
+                            <ul className='portfolio-links'>
+                                <li><a href="https://caitlingreer.github.io/CocktailHour/">Live</a></li>
+                                <li><a href="https://github.com/CaitlinGreer/CocktailHour.git">GitHub Repo</a></li>
+                            </ul>
                         </div>
-
                     </div>
+
                 </div>
+            </div>
 
         )
     }
